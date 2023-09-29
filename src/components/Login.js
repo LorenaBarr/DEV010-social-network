a3aa25e9b46d1333d5809183d5209fa2658f9d39
Betzy <betzy4291@gmail.com>
2023-09-29 18:59:35 -0500

    Iteraciones y orden de const

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,42 +3,37 @@ import { login } from '../lib/FireBase';
 export const Login = (onNavigate) => {
   const HomeDiv = document.createElement('div');
   const loginForm = document.createElement('form');
+  const buttonHome = document.createElement('button');
+  const inputEmail = document.createElement('input');
+  const inputPassword = document.createElement('input');
+  const btnLogin = document.createElement('button');
+
   HomeDiv.textContent = 'Welcome to TechShare';
   document.body.classList.add('login-body');
-  const buttonHome = document.createElement('button');
-
+  
   buttonHome.textContent = 'Return to Home';
-
   buttonHome.addEventListener('click', () => onNavigate('/'));
-
   HomeDiv.appendChild(buttonHome);
-
-  const inputEmail = document.createElement('input');
+  
   inputEmail.type = 'email';
   inputEmail.placeholder = 'Email';
   inputEmail.classList.add('email');
-
   HomeDiv.appendChild(inputEmail);
 
-  const inputPassword = document.createElement('input');
   inputPassword.type = 'password';
   inputPassword.placeholder = 'Password';
   inputPassword.classList.add('password');
-
   HomeDiv.appendChild(inputPassword);
 
-  const btnLogin = document.createElement('button');
   btnLogin.textContent = 'Log In';
   btnLogin.classList.add('btnTechShare');
-
   HomeDiv.appendChild(btnLogin);
 
   loginForm.appendChild(inputEmail);
   loginForm.appendChild(inputPassword);
   loginForm.appendChild(btnLogin);
-
   HomeDiv.appendChild(loginForm);
-
+  
   loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
