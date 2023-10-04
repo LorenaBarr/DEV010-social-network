834a60f8a45fbc7bf54fa3a16567a4bf7a4a66c1
LoreBarrera <karenlbarrera@gmail.com>
2023-10-04 14:44:41 -0500

    nueva rama para css feed

--- test/index.spec.js
+++
@@ -1,8 +0,0 @@
-// importamos la funcion que vamos a testear
-import { } from '../src/lib/index';
-
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
-  });
-});
