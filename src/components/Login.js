6ceb3cbb12db5b9079f72685652345ae5e2aea71
Knelia <knelia1989@gmail.com>
2023-10-06 20:27:11 -0500

    Se modifica append a home, login y regis

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,46 +2,35 @@ import { login } from '../lib/FireBase';
 
 export const Login = (onNavigate) => {
   const HomeDiv = document.createElement('div');
-  const welcomeTS = document.createElement('span');
-
   const loginForm = document.createElement('form');
   const imgHome = document.createElement('img');
   const buttonHome = document.createElement('button');
-  const iconHome = document.createElement('i');
   const inputEmail = document.createElement('input');
   const inputPassword = document.createElement('input');
   const btnLogin = document.createElement('button');
-
-  welcomeTS.id = 'titleLogin';
-  welcomeTS.classList.add('bienvenida');
-  HomeDiv.appendChild(welcomeTS);
 
   document.body.classList.add('login-body');
 
   buttonHome.id = 'buttonHome';
   buttonHome.classList.add('btnHome');
-  iconHome.className = 'fas fa-arrow-left';
-  buttonHome.appendChild(iconHome);
   buttonHome.appendChild(document.createTextNode('Return'));
   document.body.appendChild(buttonHome);
-  imgHome.appendChild(buttonHome);
   HomeDiv.appendChild(buttonHome);
 
   buttonHome.addEventListener('click', () => onNavigate('/'));
-  HomeDiv.appendChild(buttonHome);
 
   loginForm.id = 'loginForm';
   inputEmail.type = 'email';
   inputEmail.placeholder = 'Email';
   inputEmail.id = 'inputEmail';
   inputEmail.classList.add('inputLogin', 'inputEm');
-  HomeDiv.appendChild(inputEmail);
 
   inputPassword.type = 'password';
   inputPassword.placeholder = 'Password';
   inputPassword.id = 'inputPassword';
   inputPassword.classList.add('inputLogin', 'inputPw');
-  HomeDiv.appendChild(inputPassword);
+
+  HomeDiv.append(inputEmail, inputPassword);
 
   btnLogin.textContent = 'Log In';
   btnLogin.id = 'buttonLogIn';
@@ -61,8 +50,8 @@ export const Login = (onNavigate) => {
   modal.classList.add('modal');
   modalContent.classList.add('modalContent');
 
-  modal.appendChild(close);
-  modal.appendChild(modalContent);
+  modal.append(close, modalContent);
+
   document.body.appendChild(modal);
 
   const openModal = () => {
@@ -88,14 +77,5 @@ export const Login = (onNavigate) => {
         openModal(error.message);
       });
   });
-
   return HomeDiv;
 };
-
-// const showAlert = (message) => {
-//   if (typeof window !== 'undefined' && window.alert) {
-//     window.alert(message);
-//   } else {
-//     console.error(message);
-//   }
-// };
